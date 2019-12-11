ab3c8291b8516b5ec83db5a221567e83778036bc
buyanov <info@saity74.ru>
2019-12-11 15:25:16 +0500

    start/stop command

---
+++ bin/autoload.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+$possibleAutoloadPaths = [
+    // after split package
+    __DIR__ . '/../vendor',
+    // dependency
+    __DIR__ . '/../../..',
+    // monorepo
+    __DIR__ . '/../../../vendor',
+];
+
+foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
+    if (is_file($possibleAutoloadPath . '/autoload.php')) {
+        require_once $possibleAutoloadPath . '/autoload.php';
+        break;
+    }
+}
